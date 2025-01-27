fd788e247d99bcf35f25a69570bd50237894850c
yenzedm <dmitrij.eremeev.99@gmail.com>
2025-01-27 15:27:36 +0300

    some change

---
+++ zabbix/windows/rabbitmq-status/README.md
@@ -0,0 +1,9 @@
+###### rabbit active connections
+1. Type: zabbix agent
+2. Item key: rabbit.active.connections
+3. Trigger expression: last(/<template_name or host>/rabbit.active.connections)\>\<number of connections\>
+***
+###### rabbit queue count
+1. Type: zabbix agent
+2. Item key: rabbit.queue.count
+3. Trigger expression: last(/<template_name or host>/rabbit.queue.count) \> \<number of queue\>
---
+++ zabbix/windows/rabbitmq-status/rabbitmq-connection-count.py
@@ -0,0 +1,19 @@
+#!C:\Users\admin\AppData\Local\Programs\Python\Python39\python.exe
+import subprocess
+import re
+
+
+result = subprocess.run(['C:\\RECFACES\\AUX_APP\\SRV\\erl\\lib\\rabbitmq_server-3.9.5\\sbin\\rabbitmqctl.bat', 'status'], capture_output=True, text=True)
+
+stdout = result.stdout
+stdeer = result.stderr
+
+if stdeer:
+    print(stdeer)
+    exit(0)
+
+pattern = r'Connection count.*'
+tmp = re.search(pattern=pattern, string=stdout)
+
+conn = int(tmp.group(0).split()[2])
+print(conn)
